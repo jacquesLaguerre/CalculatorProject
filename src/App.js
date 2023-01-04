ca4c957e2fc3f85af060f9b4214d0f6003e1d608
Jacques Laguerre <jacquesblaguerre@gmail.com>
2023-01-04 18:55:48 -0500

    skeleton of the calculator

--- src/App.js
+++ src/App.js
@@ -6,10 +6,32 @@ const App = () =>  {
   return (
     <div className="App">
  <div className="calc-grid"> 
+
+ <div className="row">
 <Button />
 <Button />
 <Button />
+
+ </div>
+ <div className="row">
 <Button />
+<Button />
+<Button />
+
+ </div>
+ <div className="row">
+<Button />
+<Button />
+<Button />
+
+ </div>
+ <div className="row">
+<Button />
+<Button />
+<Button />
+
+ </div>
+
  </div>
     </div>
   );
