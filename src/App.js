81dbaa8b6895a935e07ee618b23b047112d8f2aa
Jacques Laguerre <jacquesblaguerre@gmail.com>
2023-01-05 17:48:38 -0500

    Added Clear functionality

--- src/App.js
+++ src/App.js
@@ -11,6 +11,11 @@ const App = () => {
     setText((text) => [...text, val + " " ]);
   } 
 
+  const resetInput = () => {
+    setText("");
+    setResult("");
+  }
+
   const buttonColor = "#f2a33c"; 
   return (
     <div className="App">
@@ -20,28 +25,28 @@ const App = () => {
           <Button symbol="7" handleClick={addToText}/>
           <Button symbol="8" handleClick={addToText}/>
           <Button symbol="9" handleClick={addToText}/>
-          <Button symbol="/" color={buttonColor} />
+          <Button symbol="/" color={buttonColor} handleClick={addToText} />
         </div>
 
         <div className="row">
           <Button symbol="4" handleClick={addToText}/>
           <Button symbol="5" handleClick={addToText}/>
           <Button symbol="6" handleClick={addToText}/>
-          <Button symbol="*" color={buttonColor} />
+          <Button symbol="*" color={buttonColor} handleClick={addToText} />
         </div>
         <div className="row">
           <Button symbol="1" handleClick={addToText}/>
           <Button symbol="2" handleClick={addToText} />
           <Button symbol="3" handleClick={addToText} />
-          <Button symbol="+" color={buttonColor} />
+          <Button symbol="+" color={buttonColor} handleClick={addToText} />
         </div>
         <div className="row">
           <Button symbol="0" handleClick={addToText}/>
           <Button symbol="."handleClick={addToText} />
           <Button symbol="=" />
-          <Button symbol="-" color={buttonColor}/>
+          <Button symbol="-" color={buttonColor} handleClick={addToText}/>
         </div>
-        <Button symbol="Clear" color="red"/>
+        <Button symbol="Clear" color="red" handleClick={resetInput}/>
       </div>
     </div>
   );
