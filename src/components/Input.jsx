3980af1c8546fd9e772342bfb017812a3bcb3744
Jacques Laguerre <jacquesblaguerre@gmail.com>
2023-01-05 16:55:15 -0500

    Made some Cssmodification

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -2,12 +2,14 @@ import "./Input.css"
 
 const Input = () => {
   return <>
-  <div className="input-wrapper"></div>
+  <div className="input-wrapper">
+
    <div className="result">
 <h1>Result</h1>
   </div>
   <div className="text"></div>
   <h3>Text</h3>
+  </div>
   </>
   
 
